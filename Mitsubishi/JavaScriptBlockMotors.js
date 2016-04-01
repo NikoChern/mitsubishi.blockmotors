a5677b2b62b3c43e910f8352121cc9224e4669eb
NikoChern <ncernavskij@gmail.com>
2016-04-01 17:04:54 +0300

    The first upload
    The first upload

---
+++ Mitsubishi/JavaScriptBlockMotors.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    // При наведении на ссылку
+    $(".HeadTabsClass").mouseenter(function () {
+
+        // Получаем ID блока, который нужно показать
+        var title = $(this).attr("title");
+
+        // Показываем блок
+        //$(this).append($(title));
+        $(title).fadeIn();
+    });
+
+    // При уходе мышки со ссылки
+    $(".HeadTabsClass").mouseleave(function () {
+
+        // Получаем ID блока, который нужно показать
+        var title = $(this).attr("title");
+
+        // Скрываем блок
+        $(title).fadeOut();
+
+    });
+
+});
